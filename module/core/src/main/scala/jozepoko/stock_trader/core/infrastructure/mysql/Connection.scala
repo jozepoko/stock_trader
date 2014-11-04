6066cb391fce296f07cdace0f4f058f22bc5699a
jozepoko <workread13@yahoo.co.jp>
2014-11-04 23:42:51 +0900

    リファクタ(未使用の変数を削除)

--- module/core/src/main/scala/jozepoko/stock_trader/core/infrastructure/mysql/Connection.scala
+++ module/core/src/main/scala/jozepoko/stock_trader/core/infrastructure/mysql/Connection.scala
@@ -5,8 +5,6 @@ import jozepoko.stock_trader.core.domain.entity.Environment
 import scalikejdbc._, async._
 
 object Connection {
-  private val SchemeName = "kabu"
-
   private val mysql = new Environment().mysql
 
   private val url = s"jdbc:mysql://${mysql.host}:${mysql.port}/${ConfigFactory.load().getString("db.default.scheme")}?useUnicode=yes&characterEncoding=UTF-8&zeroDateTimeBehavior=exception&tinyInt1isBit=false"
