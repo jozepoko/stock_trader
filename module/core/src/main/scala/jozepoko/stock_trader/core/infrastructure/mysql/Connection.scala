f616db8db1f4d10582fbb9dde2259df1d1e7dc1c
jozepoko <workread13@yahoo.co.jp>
2014-11-05 08:32:07 +0900

    リファクタリング(Environmentエンティティが不要だったので削除)

--- module/core/src/main/scala/jozepoko/stock_trader/core/infrastructure/mysql/Connection.scala
+++ module/core/src/main/scala/jozepoko/stock_trader/core/infrastructure/mysql/Connection.scala
@@ -1,18 +1,22 @@
 package jozepoko.stock_trader.core.infrastructure.mysql
 
 import com.typesafe.config.ConfigFactory
-import jozepoko.stock_trader.core.domain.entity.Environment
 import scalikejdbc._, async._
 
 object Connection {
-  private val mysql = new Environment().mysql
+  private val config = ConfigFactory.load()
+  private val scheme = config.getString("db.default.scheme")
+  private val user = config.getString("db.default.user")
+  private val password = config.getString("db.default.password")
+  private val host = config.getString("db.default.host")
+  private val port = config.getInt("db.default.port")
 
-  private val url = s"jdbc:mysql://${mysql.host}:${mysql.port}/${ConfigFactory.load().getString("db.default.scheme")}?useUnicode=yes&characterEncoding=UTF-8&zeroDateTimeBehavior=exception&tinyInt1isBit=false"
+  private val url = s"jdbc:mysql://$host:$port/$scheme?useUnicode=yes&characterEncoding=UTF-8&zeroDateTimeBehavior=exception&tinyInt1isBit=false"
 
   AsyncConnectionPool.add(
     'stock,
     url,
-    mysql.user,
-    mysql.password
+    user,
+    password
   )
 }
--- module/stock_price_complementer/src/main/scala/jozepoko/stock_trader/stock_price_complementer/application/Main.scala
+++ module/stock_price_complementer/src/main/scala/jozepoko/stock_trader/stock_price_complementer/application/Main.scala
@@ -1,17 +1,6 @@
 package jozepoko.stock_trader.stock_price_complementer.application
 
-import jozepoko.stock_trader.core.domain.entity.Environment
-import jozepoko.stock_trader.core.infrastructure.mysql.Connection
-
 object Main {
   def main(args: Array[String]): Unit = {
-    val environment = new Environment()
-    println(environment.manexLogin.id)
-    println(environment.manexLogin.password)
-    println(environment.mysql.user)
-    println(environment.mysql.password)
-    println(environment.mysql.host)
-    println(environment.mysql.port)
-    Connection
   }
 }
