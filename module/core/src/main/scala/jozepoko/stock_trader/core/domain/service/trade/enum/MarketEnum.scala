44f7dd6d87ca38fec53f782820b4786bf5c2fc6f
jozepoko <workread13@yahoo.co.jp>
2014-11-15 14:09:05 +0900

    MarketEnumに東証一部以外のマーケットを追加

--- module/core/src/main/scala/jozepoko/stock_trader/core/domain/service/trade/enum/MarketEnum.scala
+++ module/core/src/main/scala/jozepoko/stock_trader/core/domain/service/trade/enum/MarketEnum.scala
@@ -1,9 +1,17 @@
 package jozepoko.stock_trader.core.domain.service.trade.enum
 
 object MarketEnum {
-  case object TousyouIchibu extends Market(1, "東証一部")
+  case object TousyouFirst extends Market(1, "東証一部")
+  case object TousyouSecond extends Market(2, "東証二部")
+  case object Jasdaq extends Market(3, "ジャスダック")
+  case object Mothers extends Market(4, "東証マザーズ")
+  case object Other extends Market(99, "その他")
 
-  val list = List(TousyouIchibu)
+  val list = List(TousyouFirst, TousyouSecond, Jasdaq, Mothers)
+
+  val listWithOther = List(TousyouFirst, TousyouSecond, Jasdaq, Mothers, Other)
+
+  def get(id: Int): Market = list.find(_.id == id).getOrElse(Other)
 
   def find(id: Int): Option[Market] = list.find(_.id == id)
 }
