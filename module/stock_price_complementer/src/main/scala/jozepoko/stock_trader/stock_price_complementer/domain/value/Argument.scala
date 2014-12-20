474c0ec964081e45ab36a8f0ac16edd72c1f6a54
jozepoko <workread13@yahoo.co.jp>
2014-12-20 12:06:37 +0900

    リファクタリング(コメント追加)

--- module/stock_price_complementer/src/main/scala/jozepoko/stock_trader/stock_price_complementer/domain/value/Argument.scala
+++ module/stock_price_complementer/src/main/scala/jozepoko/stock_trader/stock_price_complementer/domain/value/Argument.scala
@@ -3,6 +3,10 @@ package jozepoko.stock_trader.stock_price_complementer.domain.value
 import jozepoko.stock_trader.stock_price_complementer.domain.service.enum.Mode
 import jozepoko.stock_trader.stock_price_complementer.domain.service.enum.ModeEnum.BeforeDay
 
+/**
+ * 引数の値を持つ値オブジェクト
+ * @param mode 実行モード
+ */
 case class Argument(
   mode: Mode = BeforeDay
 )
