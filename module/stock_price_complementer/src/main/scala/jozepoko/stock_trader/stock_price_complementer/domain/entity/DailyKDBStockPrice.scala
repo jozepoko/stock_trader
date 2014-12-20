745f4de14c9ab8554adca2c7a2cfe4efe1430212
jozepoko <workread13@yahoo.co.jp>
2014-12-20 12:09:42 +0900

    リファクタリング(コメント追加)

--- module/stock_price_complementer/src/main/scala/jozepoko/stock_trader/stock_price_complementer/domain/entity/DailyKDBStockPrice.scala
+++ module/stock_price_complementer/src/main/scala/jozepoko/stock_trader/stock_price_complementer/domain/entity/DailyKDBStockPrice.scala
@@ -2,7 +2,20 @@ package jozepoko.stock_trader.stock_price_complementer.domain.entity
 
 import jozepoko.stock_trader.core.domain.service.trade.enum.Market
 
-//コード,市場,銘柄名,業種,始値,高値,安値,終値,出来高,売買代金
+/**
+ * 株価データダウンロードサイトの、日毎の株価を表すエンティティ。
+ * idはcode。
+ * @param code 株コード
+ * @param market 市場
+ * @param name 銘柄名
+ * @param businessType 業種
+ * @param openingPrice 始値
+ * @param highPrice 高値
+ * @param lowPrice 安値
+ * @param closingPrice 終値
+ * @param turnover 出来高
+ * @param salesValue 売買代金
+ */
 case class DailyKDBStockPrice(
   code: Int,
   market: Market,
