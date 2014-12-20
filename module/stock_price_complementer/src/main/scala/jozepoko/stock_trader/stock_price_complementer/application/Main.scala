368be959f6d687d57dc731fe1b165726392953b2
jozepoko <workread13@yahoo.co.jp>
2014-12-20 12:05:32 +0900

    余分なコメント削除

--- module/stock_price_complementer/src/main/scala/jozepoko/stock_trader/stock_price_complementer/application/Main.scala
+++ module/stock_price_complementer/src/main/scala/jozepoko/stock_trader/stock_price_complementer/application/Main.scala
@@ -4,45 +4,6 @@ import jozepoko.stock_trader.stock_price_complementer.domain.service.StockPriceC
 
 object Main {
   def main(args: Array[String]): Unit = {
-    val result = new StockPriceComplementer().run(args)
-//    val connection = Connection.connection
-//    val dao = new MinutelyStockPriceDao
-//    val datetime = new DateTime(2014, 10, 11, 1, 2, 3, 0)
-//    val code = 1234
-//    val market = MarketEnum.TousyouFirst
-//    val minutelyStockPrice = MinutelyStockPrice(
-//      datetime,
-//      code,
-//      market,
-//      "hoge",
-//      1,
-//      2,
-//      3,
-//      4
-//    )
-//    val result = connection.localTx { implicit tx =>
-//      for {
-//        price <- dao.create(minutelyStockPrice)
-//      } yield {
-//        println("bbbbbbbbbbbbbbbbb")
-//        price
-//      }
-//    }
-//
-//
-//    val delete = result.map { r =>
-//      dao.delete(datetime, code, market)
-//    }
-//    delete onSuccess {
-//      case e => println("cccccccccccccccccccc")
-//    }
-//
-//    println("aaaaaaaaaaaaaa")
-//    Await.result(result, 5.second)
-//    try{
-//      new StockPriceComplementer().run(args)
-//    } finally {
-//      sys.exit()
-//    }
+    new StockPriceComplementer().run(args)
   }
 }
